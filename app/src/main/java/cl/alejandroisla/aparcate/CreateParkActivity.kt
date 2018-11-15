bea8dcae2fafa4673c05c4e7788765dbb08fc442
willywes <willywes@hotmail.cl>
2018-11-15 00:05:05 -0300

    Initial commit

---
+++ app/src/main/java/cl/alejandroisla/aparcate/CreateParkActivity.kt
@@ -0,0 +1,12 @@
+package cl.alejandroisla.aparcate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateParkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_park)
+    }
+}
